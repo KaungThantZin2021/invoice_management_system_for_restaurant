cbef291a40853f1047d254a278aeb2131de5104e
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-08-25 13:34:29 +0630

    [Fix] invoice ui

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -9,12 +9,15 @@
         }
 
         table {
-            width: 100%;
-            border-bottom: 1px solid black;
+            width: 50%;
         }
 
         td {
             font-size: 15px;
+        }
+
+        .border-bottom {
+            border-bottom: 1px solid black;
         }
 
         .m-5 {
@@ -54,8 +57,14 @@
         $tax = 0;
         $total_amount = $total_price + $tax;
     @endphp
-    <h3 class="text-center">{{ config('app.name') }}</h3>
-    <table class="m-5">
+    <table>
+        <tr>
+            <td class="text-center">
+                <h3>{{ config('app.name') }}</h3>
+            </td>
+        </tr>
+    </table>
+    <table class="m-5 border-bottom">
         <tr class="d-flex justify-content-between">
             <td class="text-left">{{ __('message.invoice_number') }}</td>
             <td class="text-right">{{ $invoice->invoice_number }}</td>
@@ -65,7 +74,7 @@
             <td class="text-right">{{ $order->order_number }}</td>
         </tr>
     </table>
-    <table class="m-5">
+    <table class="m-5 border-bottom">
         @foreach ($order_items as $key => $order_item)
         <tr class="d-flex justify-content-between">
             <td class="text-left">{{ $key + 1 }} . {{ $order_item->product->name }}</td>
@@ -74,7 +83,7 @@
         </tr>
         @endforeach
     </table>
-    <table class="m-5">
+    <table class="m-5 border-bottom">
         <tr class="d-flex justify-content-between">
             <td class="text-left">{{ __('message.total_product') }}</td>
             <td class="text-right">{{ number_format($total_product) }}</td>
@@ -84,7 +93,7 @@
             <td class="text-right">{{ number_format($total_quantity) }}</td>
         </tr>
     </table>
-    <table class="m-5">
+    <table class="m-5 border-bottom">
         <tr class="d-flex justify-content-between">
             <td class="text-left">{{ __('message.total_price') }}</td>
             <td class="text-right">{{ number_format($total_price) }} {{ __('message.mmk') }}</td>
@@ -94,12 +103,16 @@
             <td class="text-right">{{ number_format($tax) }} {{ __('message.mmk') }}</td>
         </tr>
     </table>
-    <table class="m-5">
+    <table class="m-5 border-bottom">
         <tr class="d-flex justify-content-between">
             <td class="text-left">{{ __('message.total_amount') }}</td>
             <td class="text-right">{{ number_format($total_amount) }} {{ __('message.mmk') }}</td>
         </tr>
     </table>
-    <p class="text-center" style="font-size: 10px; margin-top: 30px;">This is computer generated document.</p>
+    <table class="m-5">
+        <tr>
+            <td class="text-center" style="font-size: 10px; margin-top: 30px;">This is computer generated document.</td>
+        </tr>
+    </table>
 </body>
 </html>
